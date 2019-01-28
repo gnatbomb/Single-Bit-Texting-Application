7ee6e8fb8c8574569764243162576a420c631b2d
nichbomb <nbombard@ualberta.ca>
2019-01-27 22:25:10 -0700

    made the 1 signal version work

--- convertsigs.c
+++ convertsigs.c
@@ -3,12 +3,15 @@
 #include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/signalfd.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/time.h>
 
-#define MAXINPUT 8000       //Longest string of characters that can be sent in a single message
-#define SLEEPTIME 500       //Microseconds between messages
-#define PAUSETIME 500       //Microseconds between pings for single bit encoding
-#define MAXDELAY 2000        //Maximum time spent waiting for double-bit encoding
+#define MAXINPUT 800       //Longest string of characters that can be sent in a single message
+#define SLEEPTIME 500        //Microseconds between messages
+#define PAUSETIME 500      //Microseconds between pings for single bit encoding
+#define MAXDELAY 2000    //Maximum time spent waiting for double-bit encoding
 
 int proc2id;
 char decode_char;
@@ -25,13 +28,16 @@ void check_print() {
     write(1, &decode_char, 1);
     decode_char = '\0';
     char_count = 0;
+   // printf("printing time\n");
   }
+  return;
 }
 
 #if !defined(SINGLE) //this is for when we have 2-bits for signals
 void sig1(int signum) {
   //sends a signal to encode a bit as 0
   check_print();
+  return;
 }
 void send_to_proc() {
   //Encodes the characters into bits, then calls sig1 and sig2 depending on the bits.
@@ -54,8 +60,8 @@ void send_to_proc() {
     }
     i++;
   }
-  printf("\n");
   memset(sending_msg, '\0', i); //Clears the array of the modified characters.
+  return;
 }
 
 #endif
@@ -63,14 +69,18 @@ void send_to_proc() {
 //The following is the code which runs when we only have a single bit of information we can send.
 
 #if defined(SINGLE) //this is for when we have only 1-bit for signals
+int remainingPause = MAXDELAY;
+int firstCall = 1;
+int pingCount = 0;
+struct timeval milliseconds; //milliseconds since last killcall.
+long int actualMilliseconds;
+long int newActualMilliseconds;
 
-int going = 0;
-int pingcount = 0;
 void send_to_proc() {
   //Encodes the characters into bits, then calls sig1 and sig2 depending on the bits.
   //This sends a signal to the other process.
   int i = 0;
-  printf("made it to send_to_proc\n");
+
   while (i<MAXINPUT) {    //Makes sure that we don't get out of bounds access from array
     unsigned char c = sending_msg[i];
     if (strcmp(&sending_msg[i], "\0") == 0) { //ends the loop if we hit a null character.
@@ -80,46 +90,50 @@ void send_to_proc() {
       int bit = (c >> j) & 1;
       if (bit == 0) {
         kill(proc2id, SIGUSR1);
-        usleep(MAXDELAY);
-      } else if (bit == 1) {
+      }
+      else{
         kill(proc2id, SIGUSR1);
         usleep(PAUSETIME);
         kill(proc2id, SIGUSR1);
-        usleep(MAXDELAY);
       }
-      usleep(SLEEPTIME);
+      usleep(MAXDELAY * 2);
     }
+    //printf("send one character\n");
     i++;
   }
-  printf("\n");
   memset(sending_msg, '\0', i); //Clears the array of the modified characters.
+  return;
 }
 
-void signalWait(){
-  int timer;
-  pingcount++;
-  if (!going) {
-    going = 1;
-    timer = MAXDELAY;
-    while(timer >= 0){
-      timer -= 1;
-      usleep(1000);
-    } 
-    going = 0;
-    if (pingcount == 2){ //we got 2 pings in the time frame, so we read that as a 1
-      decode_char = decode_char | (1 << (7-char_count));
-    }
-    /*if (pingcount >= 3){
-      //RAISE AN ERROR HERE, we got 3 or more pings but only expected 1 or 2
-      pass;
-    }*/
-    pingcount = 0;
-    check_print();
+void printcall(){
+  if (pingCount >= 2){
+    decode_char = decode_char | (1 << (7-char_count));
   }
+  gettimeofday(&milliseconds, NULL);
+  actualMilliseconds = (milliseconds.tv_sec * 1000) + (milliseconds.tv_usec/1000);
+  check_print();
+  pingCount = 0;
+  return;
 }
 
 void sig1(int signum) {
-  signalWait();
+  if (firstCall){
+    gettimeofday(&milliseconds, NULL);
+    actualMilliseconds = (milliseconds.tv_sec * 1000) + (milliseconds.tv_usec/1000);
+    firstCall = 0;
+  }
+  gettimeofday(&milliseconds, NULL);
+  newActualMilliseconds = (milliseconds.tv_sec * 1000) + (milliseconds.tv_usec/1000);
+ /* printf("newActualMilliseconds = %li \nactualMilliseconds = %li\nFirst minus second = %li\n\n", newActualMilliseconds, 
+                                                                                          actualMilliseconds,
+                                                                                          (newActualMilliseconds - actualMilliseconds));
+                                                                                          */
+  if ((newActualMilliseconds - actualMilliseconds) > MAXDELAY/1000){
+    printcall();
+  }
+  pingCount++;
+
+  return;
 }
 #endif
 
@@ -127,17 +141,18 @@ void sig2(int signum) {
   //sends a signal to encode a bit as 1
   decode_char = decode_char | (1 << (7-char_count));
   check_print();
+  return;
 }
 
 
 
 int main(void) {
   //Infinite loop function. Grabs user input then calls helper functions.
-  signal(SIGUSR1, sig1); //assigns sig1 to SIGUSR1
+  //signal(SIGUSR1, sig1); //assigns sig1 to SIGUSR1
+  signal(SIGUSR1, sig1);
   signal(SIGUSR2, sig2); //assigns sig2 to SIGUSR2
   printf("Own PID: %d\nEnter partner's PID: ", getpid());  //prints own pid
   scanf("%d", &proc2id);              //asks for other process's pid
-
   while(1) {  //infinite loop until we get a .
     scanf("%[^\n]", sending_msg);
     getchar();
