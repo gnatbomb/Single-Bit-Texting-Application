874d326603782fc1775ece9c9b0757dc8b9db7bd
Muhammad Usman <usman1@ualberta.ca>
2019-02-01 10:54:44 -0700

    Code reduction and cleanup

--- convertsigs.c
+++ convertsigs.c
@@ -1,178 +1,164 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
-#include <sys/signalfd.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <sys/time.h>
 
-#define MAXINPUT 800000       //Longest string of characters that can be sent in a single message
-#define SLEEPTIME 500         //Microseconds between messages
-#define PAUSETIME 50          //Microseconds between pings for single bit encoding
-#define MAXDELAY 2000         //Maximum time spent waiting for double-bit encoding
-#define ONEVALUE 6            //number of pings to represent a 1
+#define MAXINPUT 4096         // Max input stream length
+#define SLEEPTIME 500         // Microseconds between messages
+#define PAUSETIME 50          // Microseconds between pings for single bit encoding
+#define MAXDELAY 2000         // Maximum time spent waiting for double-bit encoding
+#define ONEVALUE 6            // Number of pings to represent a 1
+
+#define SINGLESIG false
+#if defined(SINGLE)           // Set SINGLESIG depending on compilation flag
+#undef SINGLESIG
+#define SINGLESIG true
+#endif
 
 int proc2id;
-char decode_char;
-int char_count = 0;
-char sending_msg[MAXINPUT];
-char rec_msg[MAXINPUT];
-int blocked;
+char decode_char;             // Character currently being decoded by receiving process
+int char_count = 0;           // Current character count in decoding process
+char sending_msg[MAXINPUT];   // Input stream for sending process
+
+// Single signal flags
+bool first_call = true;       // Used to initialize timer
+int ping_count = 0;           // Tracks # of signals sent for precision
+struct timeval curr_time;     // Milliseconds since last killcall.
+long old_ms, new_ms;
 
 
-
+/*
+  Helper method for signal handler. Keeps track of bitcount for
+  current character. If the current bitcount is equal to 8, then
+  the current character has been built. Then, the character is
+  ready to be interpreted, and this function prints it to the
+  receiving process
+*/
 void check_print() {
-  /*helper method for signal handler. Keeps track of bitcount for current character.
-  if the current bitcount is equal to 8, then the current character has been built.
-  Then, the character is ready to be interpreted, and this function prints it to the receiving process*/
   if (++char_count == 8) {
     write(1, &decode_char, 1);
     decode_char = '\0';
     char_count = 0;
-   // printf("printing time\n");
   }
-  return;
 }
 
-#if !defined(SINGLE) //this is for when we have 2-bits for signals
-void sig1(int signum) {
-  //sends a signal to encode a bit as 0
-  check_print();
-  return;
-}
+/*
+  Encodes the character buffer/stream into bits, while
+  simultaneously signalling to a receiving process the
+  corresponding signal message
+*/
 void send_to_proc() {
-  //Encodes the characters into bits, then calls sig1 and sig2 depending on the bits.
-  //This sends a signal to the other process.
   int i = 0;
-  while (i<MAXINPUT) {    //Makes sure that we don't get out of bounds access from array
+  while (i<MAXINPUT) {
     unsigned char c = sending_msg[i];
-    if (strcmp(&sending_msg[i], "\0") == 0) { //ends the loop if we hit a null character.
-
+    // Null character termination check
+    if (strcmp(&sending_msg[i], "\0") == 0) {
       break;
     }
-    for (int j=7; j>=0; j--) { //Decodes a single character into individual bits.
-      int bit = (c >> j) & 1;
-      if (bit == 0) {
-        kill(proc2id, SIGUSR1);
-      } else if (bit == 1) {
-        kill(proc2id, SIGUSR2);
+    
+    if (!SINGLESIG) {
+      // For each bit--
+      for (int j=7; j>=0; j--) {
+	int bit = (c >> j) & 1;
+	if (bit == 0) {
+	  kill(proc2id, SIGUSR1);
+	} else if (bit == 1) {
+	  kill(proc2id, SIGUSR2);
+	}
+	usleep(SLEEPTIME);
       }
-      usleep(SLEEPTIME);
-    }
-    i++;
-  }
-  memset(sending_msg, '\0', i); //Clears the array of the modified characters.
-  return;
-}
-
-#endif
-
-//The following is the code which runs when we only have a single bit of information we can send.
-
-#if defined(SINGLE) //this is for when we have only 1-bit for signals
-int remainingPause = MAXDELAY;
-int firstCall = 1;
-int pingCount = 0;
-struct timeval milliseconds; //milliseconds since last killcall.
-long int actualMilliseconds;
-long int newActualMilliseconds;
-
-void send_to_proc() {
-  //Encodes the characters into bits, then calls sig1 and sig2 depending on the bits.
-  //This sends a signal to the other process.
-  int i = 0;
-
-  while (i<MAXINPUT) {    //Makes sure that we don't get out of bounds access from array
-    unsigned char c = sending_msg[i];
-    if (strcmp(&sending_msg[i], "\0") == 0) { //ends the loop if we hit a null character.
-      break;
-    }
-    for (int j=7; j>=0; j--) { //Decodes a single character into individual bits.
-      int bit = (c >> j) & 1;
-      if (bit == 0) {
-        for(int i = 0; i < (ONEVALUE / 2); i++){
-          usleep(PAUSETIME);
-          kill(proc2id, SIGUSR1);
-        }
+      i++;
+      
+    } else if (SINGLESIG) {
+      for (int j=7; j>=0; j--) {
+	int bit = (c >> j) & 1;
+	// Each bit sends multiple sigusr1s: bit 0 sends a few,
+	// bit 1 sends many. The receiving process deciphers
+	// the bit depending on the ping count.
+	if (bit == 0) {
+	  for(int i = 0; i < (ONEVALUE / 2); i++){
+	    usleep(PAUSETIME);
+	    kill(proc2id, SIGUSR1);
+	  }
+	}
+	else{
+	  for(int i = 0; i < (2 * ONEVALUE); i++){
+	    usleep(PAUSETIME);
+	    kill(proc2id, SIGUSR1);
+	  }
+	}
+	usleep(MAXDELAY * 2);
       }
-      else{
-        for(int i = 0; i < (2 * ONEVALUE); i++){
-          usleep(PAUSETIME);
-          kill(proc2id, SIGUSR1);
-        }
-      }
-      usleep(MAXDELAY * 2);
+      i++;
     }
-    //printf("send one character\n");
-    i++;
   }
-  memset(sending_msg, '\0', i); //Clears the array of the modified characters.
-  return;
-}
-
-void printcall(){
-  if (pingCount >= ONEVALUE){
-    decode_char = decode_char | (1 << (7-char_count));
-  }
-  gettimeofday(&milliseconds, NULL);
-  actualMilliseconds = (milliseconds.tv_sec * 1000) + (milliseconds.tv_usec/1000);
-  check_print();
-  pingCount = 0;
+  // Clear the input stream
+  memset(sending_msg, '\0', i);
   return;
 }
 
 void sig1(int signum) {
-  blocked = 1;
-  if (firstCall){
-    gettimeofday(&milliseconds, NULL);
-    actualMilliseconds = (milliseconds.tv_sec * 1000) + (milliseconds.tv_usec/1000);
-    firstCall = 0;
+  if (!SINGLESIG) {
+    // Update only char_count
+    check_print();
+    return;
+    
+  } else if (SINGLESIG) {
+    if (first_call) {
+      // Set initial base time
+      gettimeofday(&curr_time, NULL);
+      old_ms  = (curr_time.tv_sec * 1000) + (curr_time.tv_usec/1000);
+      first_call = false;
+    }
+    // Get current time to check if the bit delay (MAXDELAY) has elapsed
+    gettimeofday(&curr_time, NULL);
+    new_ms = (curr_time.tv_sec * 1000) + (curr_time.tv_usec/1000);
+    if ((new_ms - old_ms) > MAXDELAY/1000) {
+      if (ping_count >= ONEVALUE){
+	decode_char = decode_char | (1 << (7-char_count));
+      }
+      // Reset base time and ping count
+      gettimeofday(&curr_time, NULL);
+      old_ms = (curr_time.tv_sec * 1000) + (curr_time.tv_usec/1000);
+      check_print();
+      ping_count = 0;
+    }
+    ping_count++;
+    return;
   }
-  gettimeofday(&milliseconds, NULL);
-  newActualMilliseconds = (milliseconds.tv_sec * 1000) + (milliseconds.tv_usec/1000);
- /* printf("newActualMilliseconds = %li \nactualMilliseconds = %li\nFirst minus second = %li\n\n", newActualMilliseconds, 
-                                                                                          actualMilliseconds,
-                                                                                          (newActualMilliseconds - actualMilliseconds));
-                                                                                          */
-  if ((newActualMilliseconds - actualMilliseconds) > MAXDELAY/1000){
-    printcall();
-  }
-  pingCount++;
-
-  return;
 }
-#endif
 
 void sig2(int signum) {
-  //sends a signal to encode a bit as 1
+  // Append a bit 1 to the current char being constructed
   decode_char = decode_char | (1 << (7-char_count));
   check_print();
   return;
 }
 
-
-
 int main(void) {
-  //Infinite loop function. Grabs user input then calls helper functions.
-  //signal(SIGUSR1, sig1); //assigns sig1 to SIGUSR1
   signal(SIGUSR1, sig1);
-  signal(SIGUSR2, sig2); //assigns sig2 to SIGUSR2
-  printf("Own PID: %d\nEnter partner's PID: ", getpid());  //prints own pid
-  scanf("%d", &proc2id);              //asks for other process's pid
+  signal(SIGUSR2, sig2);
+  
+  printf("Own PID: %d\nEnter partner's PID: ", getpid());
+  scanf("%d", &proc2id);
   sleep(1);
-  while(1) {  //infinite loop until we get a .
+  
+  while(1) {
+    // Scan until a newline
     scanf("%[^\n]", sending_msg);
-    while (blocked);
     getchar();
-    sending_msg[strlen(sending_msg)] = '\n';
-    sending_msg[strlen(sending_msg)] = '\n';
-    if(strcmp(&sending_msg[0], ".") == 0) {
+
+    // Message termination check
+    if(strcmp(&sending_msg[0], ".") == 0)
       break;
-    } else {
-      send_to_proc();
-    }
+
+    // Append a newline and offload to send_to_proc
+    sending_msg[strlen(sending_msg)] = '\n';
+    if (SINGLESIG)
+      sending_msg[strlen(sending_msg)] = '\n';
+    send_to_proc();
   }
   return 0;
 }
